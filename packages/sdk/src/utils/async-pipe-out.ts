4723b67743a14a578928ac9acbd8418bbaa114c5
Aike Hillbrands <aike@scopas.io>
2022-03-24 16:45:36 +0100

    Remove project creation from SDK

--- packages/sdk/src/utils/async-pipe-out.ts
+++
@@ -1,7 +0,0 @@
-import { ExecaChildProcess } from 'execa'
-
-export async function asyncPipeOut(childProcess: ExecaChildProcess) {
-  childProcess.stderr?.pipe(process.stderr)
-  childProcess.stdout?.pipe(process.stdout)
-  await childProcess
-}
